d2b6d579e1d910016d797d931734e685ae752d3f
Robert Haji <roberthadji66@gmail.com>
2025-06-23 23:05:44 +0300

    adds-models.py and README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+Database (with Sqlalchemy) Late Show Challenge
+- install pipenv install flask-restful flask-sqlalchemy flask-migrate 
+SQLAlchemy-serializer
+'flask db --help'
+Init migrations with 'flask db init' --run only once
+Create or autogenerate migrations files with 'flask db migrate -m'
+Apply our migrations with flask 'flask db upgrade'
---
+++ model.py
@@ -0,0 +1,63 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy_serializer import SerializerMixin
+
+# 1. Defines naming convention
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
+# 2. Create a metadata object with the naming convention
+metadata = MetaData(naming_convention=convention)
+
+# Initialize SQLAlchemy with the metadata
+db = SQLAlchemy(metadata=metadata)
+
+
+class Episode(db.Model, SerializerMixin):
+    __tablename__ = "episodes"
+
+    serialize_rules = ("-appearances.episode",)  # This prevents recursion
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(50), nullable=False)
+    number = db.Column(db.Integer, nullable=False)
+
+    appearances = db.relationship(
+        "Appearance", back_populates="episode", cascade="all, delete-orphan"
+    )
+
+
+class Guest(db.Model, SerializerMixin):
+    __tablename__ = "guests"
+
+    serialize_rules = ("-appearances.guest",)  # This prevents recursion
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    occupation = db.Column(db.String(50))
+
+    appearances = db.relationship(
+        "Appearance", back_populates="guest", cascade="all, delete-orphan"
+    )
+
+
+class Appearance(db.Model, SerializerMixin):
+    __tablename__ = "appearances"
+
+    serialize_rules = (
+        "-guest.appearances",
+        "-episode.appearances",
+    )  # This prevents recursion
+
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer)
+    episode_id = db.Column(db.Integer, db.ForeignKey("episodes.id"), nullable=False)
+    guest_id = db.Column(db.Integer, db.ForeignKey("guests.id"), nullable=False)
+
+    episode = db.relationship("Episode", back_populates="appearances")
+    guest = db.relationship("Guest", back_populates="appearances")
